f992b2d9cfd8a24e03b9a833ddc146f76e00a236
carloshenriqueok <ifcarloshenrique@gmail.com>
2026-03-04 13:17:59 -0300

    feat: criando componente 'AppHeader'

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        default: 'Meu App',
+    },
+
+    showBack: Boolean
+});
+
+const emit = defineEmits(['back'])
+</script>
+
+<template>
+    <header class="app-header">
+        <button class="btn-back" v-if="showBack">
+            <span class="mdi mdi-keyboard-backspace"></span>
+        </button>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+import '@mdi/font/css/materialdesignicons.css';
+
 import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';
+
 
 createApp(App).mount('#app');

7295a1cfea257c670c44b23aaeeecd91edc6a3fe
carloshenriqueok <ifcarloshenrique@gmail.com>
2026-03-05 18:11:06 -0300

    FEAT: criando composables de expenses

---
+++ src/composables/useExpenses.js
@@ -0,0 +1,63 @@
+import { computed, ref } from 'vue';
+
+const expenses = ref([]);
+
+const STORAGE_KEY = 'expenses';
+
+// const filtered = computed(() => {
+//     if (filter.value === 'all') {
+//         return expenses.value;
+//     }
+//     return expenses.value.filter((item) => item.category === filter.value);
+// });
+
+function loadStorage() {
+    const stored = localStorage.getItem(STORAGE_KEY);
+    if(stored) {
+        expenses.value = JSON.parse(stored);
+    }
+}
+
+function saveStorage() {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(expenses.value));
+};
+
+loadStorage();
+
+export function useExpense(){
+    const totalExp = computed(() => {
+        return expenses.value.reduce((sum, item) => sum + Number(item.price.replace(',' , '.') || 0), 0);
+    });
+
+    function addExpense(expense) {
+        const newExpense = {
+            id: Date.now(),
+            ...expense,
+            createdAt: new Date().toISOString(),
+        };
+        expenses.value.unshift(newExpense);
+        saveStorage();
+        return newExpense;
+    }
+
+    function deleteExpense(id) {
+        expenses.value = expenses.value.filter((item) => item.id !== id);
+        saveStorage();
+    };
+
+    function clearAll() {
+        if(!confirm('Tem certeza que deseja excluir tudo??')) {
+            return
+        }
+        expenses.value = []
+        saveStorage()
+    }
+
+    return {
+        totalExp,
+        addExpense,
+        deleteExpense,
+        clearAll,
+        expenses,
+    }
+}

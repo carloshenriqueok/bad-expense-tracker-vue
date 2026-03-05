4dc665ef1b21555b15165fd871b2bc1280c677a5
carloshenriqueok <ifcarloshenrique@gmail.com>
2026-03-05 18:43:38 -0300

    FEAT: criando componente de input

---
+++ src/components/forms/AppInput.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    modelValue: [String, Number],
+    label: String,
+    type: {
+        type: String,
+        default: 'text'
+    },
+    placeholder: String,
+    required: Boolean
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const value = computed({
+    get: () => props.modelValue,
+    set: (val) => emit('update:modelValue', val)
+})
+</script>
+
+<template>
+    <div class="app-input">
+        <label class="label">
+            {{ label }}
+            <span v-if="required" class="required">*</span>
+        </label>
+        <input
+            v-model="value"
+            :type="type"
+            :required="required"
+            :placeholder="placeholder"
+            class="input"
+        />
+    </div>
+</template>
+
+<style scoped>
+.app-input {
+    margin-bottom: 10px;
+}
+
+.label {
+    display: block;
+    margin-bottom: 8px;
+    font-size: 18px;
+    font-weight: 600;
+}
+
+.required {
+    color: red;
+}
+
+.input {
+    width: 100%;
+    min-height: 20px;
+    padding: 10px 16px;
+    font-size: 16px;
+    border: 2px solid #ddd;
+    border-radius: 8px;
+    background: white;
+    transition: border-color 0.2s;
+}
+
+.input:focus {
+    outline: none;
+    border-color: #659C07;
+}
+
+</style>
